631d66450772d3b1bece666a778b912ac99e67f9
johnnque <johnn_patrick_que@dlsu.edu.ph>
2026-03-28 15:36:09 +0800

    TMR0 initialize and also overflow reset
    #1 for now overflow = 1ms, not sure if thats final

---
+++ TMR0.c
@@ -0,0 +1,21 @@
+#include <xc.h>
+#include <stdint.h>
+
+void TMR0_init(void){
+    // Tick/sec = Crystal / (Prescaler * 4)     = 125000   
+    //125000/s * 1s/1000ms                      = 125tick/ms
+
+    // 256-125  ticks                           = 131  
+
+    OPTION_REG = 0x02;
+
+    TMR0 = 131;
+    INTCON |= 0xA0 
+    return;
+}
+
+void TMR0_overflowReset(void){
+    TMR0 = 131;
+    INTCON &= ~0x04;
+    return;
+}
